ae9cab3ed527502fb4be712da54ad987cca5bf54
Ahlam-fahym <ahlamfhym80@gmail.com>
2022-11-18 22:24:18 +0200

    oppp

--- website/app.js
+++ website/app.js
@@ -1,3 +1,4 @@
+// global
 let d = new Date();
 let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
 
@@ -9,33 +10,38 @@ const date = document.getElementById("date");
 const temp = document.getElementById("temp");
 const content = document.getElementById("content");
 const error = document.getElementById("error");
-// Create a new date instance dynamically with JS
 
+// event listener to add function to existing html dom element
 document.getElementById("generate").addEventListener("click", function () {
+
+  // post data ti api
   const zipCode = document.getElementById("zip").value;
   const feelingCode = document.getElementById("feelings").value;
 
+// post data for git zipCodeINformation
   getCodeInformation(zipCode)
     .then((teem) => {
       console.log(teem);
       if (teem) {
         const {
           main: { temp },
-          name: city,
         } = teem;
-        const inff = { newDate,city, temp, feelingCode };
+        // post data to server and saving
+        const inff = { newDate, temp, feelingCode };
         post(apiUrl + "/postData", inff);
       }
     })
-    .then(() => updateUI());
-});
+    .then(() => updateUI());// update UI
 
+});
+// git zipCode information from api
 async function getCodeInformation(zipCode){
   try {
     const res = await fetch(baseUrl + zipCode + apiKey);
     const teem = await res.json();
-
+// if data true and if data false
     if (teem.cod != 200) {
+// send error in HTML
       error.innerHTML = data.message;
       setTimeout((_) => (error.innerHTML = ""), 2000);
     }
@@ -44,7 +50,7 @@ async function getCodeInformation(zipCode){
     console.log(error);
   }
 };
-
+// send data to server in post
 const post = async (url = "", inff = {}) => {
   const res = await fetch(url, {
     method: "POST",
@@ -56,12 +62,14 @@ const post = async (url = "", inff = {}) => {
     const newData = await res.json();
     console.log(`you date`, newData);
     return newData;
+    // error
   } catch (error) {
     console.log(error);
   }
 };
-
+// update UI
 async function updateUI(){
+  // git data from server 
   const res = await fetch(apiUrl + "/getAll");
   try {
     const saveData = await res.json();
